0a72f6ae373aec7888309d3eae8301ab4279e582
shreejanmdr <shreejan0497@gmail.com>
2024-06-10 21:26:31 +0545

    .

--- lib/features/course/domain/usecases/course_usecase.dart
+++ lib/features/course/domain/usecases/course_usecase.dart
@@ -1,7 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:student_management_starter/core/failure/failure.dart';
-import 'package:student_management_starter/features/batch/domain/entity/batch_entity.dart';
 import 'package:student_management_starter/features/course/domain/entity/course_entity.dart';
 import 'package:student_management_starter/features/course/domain/repository/course_repository.dart';
 
@@ -27,6 +26,4 @@ class CourseUseCase {
   Future<Either<Failure, bool>> deleteCourse(String id) async {
     return courseRepository.deleteCourse(id);
   }
-
-  
 }
